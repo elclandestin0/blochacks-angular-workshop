7559411e5277c508cb6b01b1df4d825999ca69b2
Memo Khoury <majd.khoury93@gmail.com>
2018-11-14 20:05:51 -0500

    dir: added components, model, services & pages

---
+++ angular-workshop/src/app/pages/homepage/homepage.component.ts
@@ -0,0 +1,16 @@
+// Angular imports
+import { Component, OnInit } from "@angular/core";
+
+// Model imports
+import { Student } from "../../model/Student/student";
+
+@Component({
+  selector: "app-homepage",
+  templateUrl: "./homepage.component.html",
+  styleUrls: ["./homepage.component.css"]
+})
+export class HomepageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
